d6916f9e08539d7e533cf08460e75c1ad89ee331
Skyler Braden <skylerbraden@me.com>
2016-09-12 22:43:20 -0500

    adding BJA

---
+++ scripts/services/SongPlayer.js
@@ -0,0 +1,169 @@
+(function() {
+    function SongPlayer($rootScope, Fixtures) {
+        var SongPlayer = {};
+        /**
+        * @desc Active album object (e.g., albumPicasso)
+        * @type {Object}
+        */
+        var currentAlbum = Fixtures.getAlbum();
+        /**
+        * @desc Buzz object audio file
+        * @type {Object}
+        */
+        var currentBuzzObject = null;
+        
+        /**
+        * @function setSong
+        * @desc Stops currently playing song and loads new audio file as currentBuzzObject
+        * @param {Object} song
+        */
+        var setSong = function(song) {
+            if (currentBuzzObject) {
+                stopSong(SongPlayer.currentSong);
+            }
+            
+            currentBuzzObject = new buzz.sound(song.audioUrl, {
+                formats: ['mp3'],
+                preload: true
+            });
+            
+            currentBuzzObject.bind('timeupdate', function() {
+                $rootScope.$apply(function() {
+                    SongPlayer.currentTime = currentBuzzObject.getTime();
+                });
+            });
+            
+            SongPlayer.currentSong = song;
+        };
+        
+        /**
+        * @function playSong
+        * @desc Plays new audio file assigned to currentBuzzObject
+        * @param {Object} song
+        */
+        var playSong = function(song) {
+            currentBuzzObject.play();
+            song.playing = true;
+        };
+              
+        /**
+        * @function stopSong
+        * @desc Stops current audio file assigned to currentBuzzObject
+        * @param {Object} song
+        */
+        var stopSong = function(song) {
+            currentBuzzObject.stop();
+            song.playing = null;
+        };
+        
+        /**
+        * @function getSongIndex
+        * @desc Gets the index of song
+        * @param {Object} song
+        */
+        var getSongIndex = function(song) {
+            return currentAlbum.songs.indexOf(song);
+        };
+        
+        /**
+        * @desc Active song object from list of songs
+        * @type {Object}
+        */
+        SongPlayer.currentSong = null;
+        
+        /**
+        * @desc Current playback time (in seconds) of currently playing song
+        * @type {Number}
+        */
+        SongPlayer.currentTime = null;
+        
+    
+        SongPlayer.play = function(song) {
+            song = song || SongPlayer.currentSong;
+            
+            if(song == null){
+                return true;
+            }
+            
+            if (SongPlayer.currentSong !== song) {
+                setSong(song);
+                // .play method is from Buzz's own library
+                playSong(song);
+            } else if (SongPlayer.currentSong === song) {
+                if (currentBuzzObject.isPaused()) {
+                    playSong(song);
+                }
+            }
+        };
+        
+        SongPlayer.pause = function(song) {
+            song = song || SongPlayer.currentSong;
+            currentBuzzObject.pause();
+            song.playing = false;
+        };
+        
+        /**
+        * @desc Gets index of the currently playing song and then decreases that index by one. [If the previous song index is less than zero (the user is on the first song and clicks previous button), stop the currently playing song, and set the value of the currently playing song to the first song. Otherwise, move to the previous song and automatically play it.]
+        * @type: {Number}
+        */
+        SongPlayer.previous = function() {
+            var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+            currentSongIndex--;
+            
+            if (currentSongIndex < 0) {
+                stopSong(SongPlayer.currentSong);
+            } else {
+                var song = currentAlbum.songs[currentSongIndex];
+                setSong(song);
+                playSong(song);
+            }
+        };
+        
+        /**
+        * @desc Gets index of the currently playing song and then increases theat index by one. [ ... ]
+        * @type: {Number}
+        */
+        SongPlayer.next = function() {
+            var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+            currentSongIndex++;
+            
+            if(currentSongIndex >= currentAlbum.songs.length) {
+                stopSong(SongPlayer.currentSong);
+            } else {
+                var song = currentAlbum.songs[currentSongIndex];
+                setSong(song);
+                playSong(song);
+            }
+        };
+        
+        /**
+        * @function setCurrentTime
+        * @desc Set current time (in seconds) of currently playing song
+        * @param {Number} time
+        */
+        SongPlayer.setCurrentTime = function(time) {
+            if (currentBuzzObject) {
+                currentBuzzObject.setTime(time);
+            }
+        };
+        
+        /**
+        * @function setVolume
+        * @desc Set current volume of currently playing song
+        * @param {Number} volume
+        */
+        SongPlayer.setVolume = function(volume) {
+            if (currentBuzzObject) {
+                currentBuzzObject.setVolume(volume);
+            }
+        };
+        
+        SongPlayer.volume = 45;
+           
+        return SongPlayer;
+    }
+    
+    angular
+        .module('blocJams')
+        .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
+})();
